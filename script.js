c519c053bbf0f6e60e717a083ef454208283a507
EricchiYukia <git@ericchi.moe>
2022-11-16 16:56:33 +0100

    Base e un po' di codice

---
+++ script.js
@@ -0,0 +1,38 @@
+// --------------------
+function palindromo(parola){
+    
+}
+
+function generaSommaRandom(min, max, numero){
+    let intervallo = max-min;
+    let caso = Math.random();
+    // Toglie decimali e prende l'input più vicino
+    caso = Math.floor(caso * intervallo);
+    caso = caso + min;
+    return numero + caso;
+}
+
+function pali(){
+    let parola = document.getElementById("inputPali").toLowerCase();
+    console.log(parola);
+}
+
+function dispari(){
+    let numero = document.getElementById("inputDispari");
+    let selDispari = document.getElementById("btnDispari");
+    let selPari = document.getElementById("btnPari");
+    let min = 1;
+    let max = 5;
+    if (numero >= min && numero <= max) {
+        let risultatoCasuale = generaSommaRandom(min, max, numero);
+        if ((risultatoCasuale) % 2 == 1 && selDispari.checked) {
+            alert("Hai vinto! È dispari! (" + risultatoCasuale + ")");
+        } else if ((risultatoCasuale) % 2 == 0 && selPari.checked) {
+            alert("Hai vinto! È pari! (" + risultatoCasuale + ")");
+        } else {
+            alert("Hai perso... (" + risultatoCasuale + ")");
+        }
+    } else {
+        alert("Non hai inserito un numero valido.");
+    }
+}
